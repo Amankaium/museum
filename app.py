8db5bf9b95cf17b2d6310c2c4c44c3335658aa7d
Amankaium <kaium@list.ru>
2020-09-01 21:39:20 +0600

    created museum

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return "<h1>Домашняя страница!</h1>"
+    # return render_template("index.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", name="Каюм")
+
+@app.route("/all")
+def all():
+    return render_template("all.html")
