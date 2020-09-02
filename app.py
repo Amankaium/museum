3a5018e2bc7e4ae2ec1d7df7c204e0f1f421fc4c
Amankaium <kaium@list.ru>
2020-09-02 22:19:11 +0600

    added all and one pages

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "<h1>Домашняя страница!</h1>"
-    # return render_template("index.html")
+    # return "<h1>Домашняя страница!</h1>"
+    return render_template("index.html")
 
 @app.route("/contact")
 def contact():
@@ -13,8 +13,31 @@ def contact():
 
 @app.route("/all")
 def all():
-    return render_template("all.html")
+    f = open("links.txt", mode="r", encoding="utf-8")
+    lst = []
+    for row in f:
+        splited = row.split()
+        number = splited[0]
+        link = splited[-1]
+        txt = row[len(number)+1 : -len(link)-1]
+        lst.append([number, txt, link])
+    f.close()
+    return render_template("all.html", lst=lst)
 
-@app.route("/one-image-page")
-def one():
-    return "Тут будет страница одной картины"
+@app.route("/one-image-page/<id>")
+def one(id):
+    f = open("links.txt", mode="r", encoding="utf-8")
+    lst = []
+    for row in f:
+        splited = row.split()
+        number = splited[0]
+        link = splited[-1]
+        txt = row[len(number)+1 : -len(link)-1]
+        lst.append([txt, link])
+    f.close()
+    try:
+        image = lst[int(id)-1]
+    except:
+        return "<h1>Такой картины нет</h1>"
+
+    return render_template("one.html", number=id, image=image)
