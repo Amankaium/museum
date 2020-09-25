5160a6d31c4372d25fc612e71d9800f9acdb93c1
Amankaium <kaium@list.ru>
2020-09-25 20:11:08 +0600

    connected database to project

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 from flask import Flask, render_template, request
 from openpyxl import load_workbook
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:postgres@localhost/museum_flask'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///D:\\kaium\\projects\\python04\\projects\\museum\\museum.db'
+
+db = SQLAlchemy(app)
+
+
+class Exhibit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    url = db.Column(db.String(500), nullable=False)
+    
+    def __repr__(self):
+        return self.name
+
+# db.create_all()
+
+@app.route("/all")
+def all():
+    lst = Exhibit.query.all()
+    return render_template("exhibits.html", lst=lst)
+
 
 @app.route("/")
 def homepage():
-    # return "<h1>Домашняя страница!</h1>"
     return render_template("index.html")
 
 @app.route("/contact", methods=["GET", "POST"])
@@ -20,8 +41,8 @@ def contact():
         user_name = "Дорогой друг"
     return render_template("contact.html", name=user_name)
 
-@app.route("/all")
-def all():
+@app.route("/txt")
+def txt():
     f = open("links.txt", mode="r", encoding="utf-8")
 
     lst = []
@@ -45,24 +66,26 @@ def excel():
     return render_template("excel.html", lst=lst)
 
 
-@app.route("/one-image-page/<id>")
+@app.route("/exhibit/<id>")
 def one(id):
-    f = open("links.txt", mode="r", encoding="utf-8")
+    # f = open("links.txt", mode="r", encoding="utf-8")
 
-    lst = []
-    for row in f:
-        splited = row.split()
-        number = splited[0]
-        link = splited[-1]
-        txt = row[len(number)+1 : -len(link)-1]
-        lst.append([txt, link])
-    f.close()
-    try:
-        image = lst[int(id)-1]
-    except:
-        return "<h1>Такой картины нет</h1>"
+    # lst = []
+    # for row in f:
+    #     splited = row.split()
+    #     number = splited[0]
+    #     link = splited[-1]
+    #     txt = row[len(number)+1 : -len(link)-1]
+    #     lst.append([txt, link])
+    # f.close()
+    # try:
+    #     image = lst[int(id)-1]
+    # except:
+    #     return "<h1>Такой картины нет</h1>"
 
-    return render_template("one.html", number=id, image=image)
+    exhibit = Exhibit.query.filter_by(id=id).first()
+
+    return render_template("one.html", exhibit=exhibit)
 
 
 @app.route("/create")
@@ -72,16 +95,44 @@ def create_form():
 
 @app.route("/add", methods=["POST"])
 def add():
+    # f = open("links.txt", mode="r+", encoding="utf-8")
+
+    # rows = f.read().split("\n")
+    # last_eksponat = rows[-2]
+    # last_eksponat_list = last_eksponat.split()
+    # last_num = int(last_eksponat_list[0])
+
+    # # f.write("7 " + form["name"] + " " + form["url"] + "\n")
+    # f.write(f"{last_num + 1} {form['name']} {form['url']}\n") # 7 Кисточки http:////art2bid.com/assets/images/slid..
+    # f.close()
     form = request.form
-    f = open("links.txt", mode="r+", encoding="utf-8")
-
-    rows = f.read().split("\n")
-    last_eksponat = rows[-2]
-    last_eksponat_list = last_eksponat.split()
-    last_num = int(last_eksponat_list[0])
-
-    # f.write("7 " + form["name"] + " " + form["url"] + "\n")
-    f.write(f"{last_num + 1} {form['name']} {form['url']}\n") # 7 Кисточки http:////art2bid.com/assets/images/slid..
-    f.close()
+    name = form["name"]
+    url = form["url"]
+    e = Exhibit(name=name, url=url)
+    db.session.add(e)
+    db.session.commit()
     return "<h2>Ваша форма обработана</h2>"
 
+
+@app.route("/exhibit/<id>/delete")
+def delete(id):
+    exhibit = Exhibit.query.filter_by(id=id).first()
+    db.session.delete(exhibit)
+    db.session.commit()
+    return "<h1>Экспонат удалён!</h1>"
+
+
+@app.route("/exhibit/<id>/update_form")
+def update_form(id):
+    exhibit = Exhibit.query.filter_by(id=id).first()
+    return render_template("update_form.html", exhibit=exhibit)
+
+@app.route("/exhibit/<id>/update", methods=["POST"])
+def update(id):
+    name = request.form["name"]
+    url = request.form["url"]
+    exhibit = Exhibit.query.filter_by(id=id).first()
+    exhibit.name = name
+    exhibit.url = url
+    db.session.commit()
+    return "<h1>Изменения сохранены!</h1>"
